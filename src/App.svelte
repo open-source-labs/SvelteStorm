9d3ccadd16ee77ce068d8c60d42ece3ae8d109f8
Sam Filip <samjfilip@gmail.com>
2021-05-25 20:00:24 -0700

    updated gitignore and fixed format errors

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,18 @@
 <script>
     import FileDir from './Directory/FileDir.svelte'
-<<<<<<< HEAD
-    import Monaco from './MonacoComponents/Monaco.svelte'
-    import { FitAddon } from 'xterm-addon-fit'
-    import { onMount } from 'svelte';
-    import './xterm.css';
-    
-    export let orientation = 'columns';
-
-    let localhost = "http://localhost:5000/"
-    let refreshed = false
-    const { remote, ipcRenderer } = require('electron');
-
-    const Terminal = require('xterm').Terminal
-
-    const fitAddon = new FitAddon();
-    const term = new Terminal();
-    
-    function onClick() {
-      refreshed = true
-      localhost = "http://localhost:5000/"
-    }
-
-    onMount(() => {
-		  console.log('the component has mounted');
-       term.setOption('cursorStyle', 'block');
-       term.setOption('cursorBlink', true);
-       term.setOption('fontSize', 14);
-       
-       term.loadAddon(fitAddon);
-        term.open(document.getElementById('xterm'));
-        fitAddon.fit();
-
-        term.write('\x1b[32mWelcome to Svelte Storm!\x1b[m\r\n');
-
-        term.onData(e => {
-            ipcRenderer.send("terminal-into", e);
-        });
-        ipcRenderer.on('terminal-incData', (event, data) => {
-            term.write(data);
-        })
-    });
-=======
     import NewTabs from './MonacoComponents/Tabs/NewTabs.svelte';
     import XTerm from './XTerm.svelte';
     
     export let orientation = 'columns';
->>>>>>> tabbedBrowsing
+
+    let localhost = "http://localhost:5000/"
+    let refreshed = false;
+    const { remote, ipcRenderer } = require('electron');
+
+    function onClick() {
+      refreshed = true
+      localhost = "http://localhost:5000/"
+    }
 
     let tabs = [];
  
--- src/MonacoComponents/Monaco.svelte
+++ src/MonacoComponents/Monaco.svelte
@@ -1,12 +1,6 @@
 <script>
   import {  afterUpdate, onMount } from 'svelte';
   import * as monaco from 'monaco-editor';
-<<<<<<< HEAD
-  import DirectoryData from '../Utilities/DirectoryStore';
-  const { remote, ipcRenderer } = require('electron');
-  const currentWindow = remote.getCurrentWindow();
-=======
->>>>>>> tabbedBrowsing
   const fs = require('fs');
 
   const { remote, ipcRenderer } = require('electron');
@@ -17,71 +11,9 @@
 
   let monEditor;
   let containerElt;
-<<<<<<< HEAD
-  let file;
-  let messageObj; 
-
-  let readData = '';
-  const unsub = DirectoryData.subscribe(data =>{
-      file = data.openFilePath
-      if(data.fileRead){
-        readData = fs.readFileSync(data.openFilePath).toString();
-        value = readData.split(/\r?\n/);
-        language = path.basename(data.openFilePath).split('.').pop()
-        let title = 'Svelte Storm';
-        if (data.openFilePath) { title = `${path.basename(data.openFilePath)} - ${title}`; }
-        currentWindow.setTitle(title);
-        monEditor.setValue(value.join('\n'))
-        monaco.editor.setModelLanguage(monEditor.getModel(),getLanguage(language))
-      }
-  });
-
-  const getLanguage = (lang) => {
-    console.log(lang)
-      switch (lang) {
-        case 'js':
-          return 'javascript';
-        case 'jsx':
-          return 'javascript';
-        case 'ts':
-          return 'typescript';
-        case 'json':
-          return 'json';
-        case 'css':
-          return 'css';
-        case 'html':
-          return 'html';
-        case 'md':
-          return 'markdown';
-        case 'svelte':
-          return 'html';
-        default:
-          return undefined;
-      }
-
-    };
-
-  ipcRenderer.on('file-opened', function (evt, file, content) {
-      console.log(content)
-      value = content.split(/\r?\n/);
-      language = file.split('.').pop();
-      let title = 'Svelte Storm';
-      if (file) { title = `${path.basename(file)} - ${title}`; }
-      currentWindow.setTitle(title);
-      console.log(value)
-      monEditor.setValue(value.join('\n'))
-      monaco.editor.setModelLanguage(monEditor.getModel(),getLanguage(language))
-      console.log(monEditor.getValue())
-  });
-=======
->>>>>>> tabbedBrowsing
 
   onMount(() => {
-    // monaco.editor.setModelLanguage(monEditor.getModel(), language)
-    // createEditor();
-    // model = monaco.editor.createModel(
-    //   value,
-    // )
+
     monEditor = monaco.editor.create(containerElt, {
       value: value.join('\n'),
       language: language,
@@ -89,15 +21,6 @@
       wordWrap: 'on',
       fontSize: "16px",
     })
-<<<<<<< HEAD
-
-    
-
-  }
-  onMount(() => {
-     createEditor()
-=======
->>>>>>> tabbedBrowsing
   })
 
 	afterUpdate(() => {
@@ -113,19 +36,11 @@
         })
       }
 	});
-<<<<<<< HEAD
   
   ipcRenderer.on('save-markdown',  function () {
       messageObj = {content : monEditor.getValue(), file : file }
       ipcRenderer.send('synchronous-message', messageObj)
     });
-=======
-
-  ipcRenderer.on('save-markdown',  function (file, content) {
-    ipcRenderer.send('synchronous-message', monEditor.getValue())
-  });
-
->>>>>>> tabbedBrowsing
 </script>
 
 <svelte:head />
