da509253545700bdf402feef57f6903a1689666f
swethakunda <92357811+swethakunda@users.noreply.github.com>
2022-02-22 09:12:41 -0800

    Revert "Sina"

--- src/App.svelte
+++ src/App.svelte
@@ -122,10 +122,9 @@
 
   .e {
     font: white;
-    overflow: hidden;
-    width:100%;
-    min-width: 300%;
-    max-width: 200%;
+    overflow: auto;
+    min-width: 10%;
+    max-width: 150%;
     min-height: 100%;
     grid-column: 2 / 4;
     grid-row: 2;
