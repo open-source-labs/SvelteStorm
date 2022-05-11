3da6115868526e3c76a12194bdf2530e3bcf21df
Ricky Johnson <rickyjohnson@Rickys-MBP.lan>
2022-05-11 09:36:26 -0400

    window-resizing working - terminal not moving to next line on input

--- src/App.svelte
+++ src/App.svelte
@@ -51,6 +51,7 @@
         </form>
       </div>
     </div>
+  <div class="middle-separator"></div>
   <div class="box wrapper-bottom">
     <div class="box c root">
       <StateManager />
@@ -67,19 +68,13 @@
     height: 100%;
     width: 100%;
   }
+  /*2022-ST-RJ Restructured CSS to use flex rather than grid so dynamic window resizing works appropriately /*
   /* Wrapper Window - SvelteTeam */
   .wrapper {
     height: 100%;
+    width: 100%;
     display: flex;
     flex-direction: column;
-    width: 100%;
-    /* flex-wrap: wrap; */
-    /* grid-template-areas: "a b b b b b b"
-                         "c e e e e e e"; */
-    /* grid-template-areas: "a a a a a a a"
-                         "b b b b b b b"; */
-    /* grid-template-columns: min-content; */
-    /* grid-template-rows: 1fr; */
     background-color: rgb(39, 38, 38);
     color: #444;
   }
@@ -91,45 +86,36 @@
     width: 98%;
     resize: vertical;
     overflow: auto;
-    /* grid-template-columns: min-content; */
-    grid-template-rows: 1fr;
     background-color: rgb(39, 38, 38);
     color: #444;
-    grid-area: a;
   }
   .wrapper-bottom {
     min-height: 10%;
-    /* max-height: 35%; */
     height: 35%;
     flex-grow: 1;
     display: flex;
-    /* flex: 1 1 auto; */
     flex-direction: row;
     width: 98%;
-    /* grid-template-columns: min-content; */
-    grid-template-rows: 1fr;
     background-color: rgb(39, 38, 38);
     color: #444;
-    grid-area: b;
   }
 
   .render-wrapper {
-    background-color: blue;
+    background-color: #252532;
     display:flex;
     flex-direction:column;
     height: 100%;
   }
-  /* .render-frame {
-    background-color: red;
-    display:flex;
-    flex-direction:column;
-    height: 100%;
-  } */
+
+  .middle-separator{
+    padding: 2px;
+  }
+
   .box {
     background-color: rgb(39, 38, 38);
     color: rgb(245, 242, 239);
     border-radius: 0px;
-    padding: 5px;
+    /* padding: 5px; */
   }
   /* File Directory - SvelteTeam */
   .a {
@@ -138,84 +124,41 @@
     resize: horizontal;
     width:10%;
     min-width:10%;
-    /* min-width: 10%;
-    max-width: 150%;
-    min-height: 10%;
-    max-height: 150%; */
-    /* grid-column: 1; */
-    /* grid-row: 1; */
+    max-width: 30%;
     padding: 0;
     background-color: rgba(28, 28, 36, 0.678);
     border-right: 1px solid #3d3d3d;
     border-bottom: 1px solid #3d3d3d;
-    grid-area: a;
   }
   
   /* Text Editor - SvelteTeam */
   .b {
-  
-      /* RJ Setting overflow to auto from scroll so resize handlers show - SvelteTeam */
-      /* overflow: scroll; */
-      overflow: auto;
-      width:45%;
-      /* min-width: 500px; */
-      /* max-width: 50vw; */
-      /* min-width: 10%; */
-      
-      /* width:90%; */
-      /* RJ Adding resize property for text editor so resize handlers show - SvelteTeam */
-      resize: horizontal;
-      grid-template-columns: 3fr;
-    /* min-width: 10%; */
-      /* RJ Adjusting min-width to 30% - SvelteTeam */
-    /* min-width: 30%;
-    max-width: 150%;
-    min-height: 10%;
-    max-height: 150%; */
-    /* grid-column: 2; */
-    /* grid-row: 1; */
+    overflow: auto;
+    width:45%;
+    resize: horizontal;
     background-color: rgba(35, 35, 65, 0.452);
     border-bottom: 1px solid #3d3d3d;
     border-right: 1px solid #3d3d3d;
-    /* grid-area: b; */
   }
     /* State Management Window - SvelteTeam */
   .c {
     overflow: auto;
     width:10%;
     min-width:10%;
-    /* min-width: 10%; */
-    /* max-width: 150%; */
-    /* min-height: 10%; */
-    /* max-height: 150%; */
-    /* grid-column: 1;
-    grid-row: 2; */
-    /* resize: both; */
-    /* width:10%; */
     background-color: rgba(28, 28, 36, 0.678);
     border-right: 1px solid #3d3d3d;
     padding: 0;
-    /* grid-area: c; */
   }
 
     /* Browser Render Window - SvelteTeam */
   .d {
-    /* overflow: auto; */
-    /* resize: both; RJ added to resize renderer window vertically */
-    /* min-width: 10%;
-    min-height: 10%;
-    max-height: 150%; */
+    min-width:30%;
     flex-direction: column;
     flex-grow: 1; /*Let render window take up remaining space in the flexbox */
-    /* width:45%; */
     padding: 0px;
     text-align: center;
-    /* grid-column: 3;
-    grid-row: 1; */
     background-color: rgba(35, 35, 65, 0.452);
     border-bottom: 1px solid #3d3d3d;
-
-    /* grid-area: d; */
   }
 
   .d input {
@@ -231,36 +174,20 @@
     font: white;
     overflow: auto;
     width: 100%;
-    /* min-width: 300%; */
-    /* min-height: 100%; */
-    /* grid-column: 2 / 5;
-    grid-row: 2; */
-    display: flex;
-    flex-direction: column;
     background-color: rgba(35, 35, 65, 0.452);
-    grid-area: e;
   }
 
-      /* Webpage Render - SvelteTeam */
+    /* Webpage Render - SvelteTeam */
     .webpage {
     overflow: auto;
-    resize: vertical;
+    /* resize: vertical; */
     height: 100%;
-    width: 100%;
-    /* min-width: 10%;
-    min-height: 10%;
-    max-height: 150%; */
-    /* padding: 0px;
-    grid-row: 1;
-    height: 500px;
-    width: 95%; */
+    width: 98%;
   }
 
   .b :global(.childClass) {
     overflow: scroll;
-    /* display: flex; */
-    /* height: 100%;
-    width: 100%; */
+    display: flex;
   }
 
   iframe:focus {
