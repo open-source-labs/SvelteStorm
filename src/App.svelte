ed99ac3586d67e13eaa5e20c6da6655a41be962d
Ricky Johnson <rickyjohnson@Rickys-MBP.lan>
2022-05-18 13:08:21 -0400

    window resize state mgmt merge

--- src/App.svelte
+++ src/App.svelte
@@ -153,6 +153,7 @@
     let filedirDivider = document.getElementById("filedir-divider");
     let statemgrDivider = document.getElementById("statemgr-divider");
 
+<<<<<<< HEAD
     horizDivider.addEventListener("mouseover", (e) =>
       chgCursor(e, "horizontal-divider")
     );
@@ -209,6 +210,13 @@
     //Need to trigger this after resize so that it follows xterm's fittaddon resize
     window.addEventListener("resize", xtermSetWidth);
   }); //End of onMount
+=======
+  xtermSetWidth();
+  //Need to trigger this after resize so that it follows xterm's fittaddon resize
+  window.addEventListener('resize', xtermSetWidth);
+}); //End of onMount
+ 
+>>>>>>> 03df4e1 (window resize state mgmt merge)
 
   const handleSubmit = () => {
     submit = false;
