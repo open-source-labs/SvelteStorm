61e869e07b458117996e71b9d811d38024cefd81
Zachary Radford <101583420+zachradf@users.noreply.github.com>
2022-05-17 23:58:00 -0400

    rebase

--- src/CodeEditor/CodeMirror.svelte
+++ src/CodeEditor/CodeMirror.svelte
@@ -45,24 +45,6 @@
     console.log(value, "is not in the docs!");
     return false;
   }
-  // const wordHover = hoverTooltip((view, pos, side) => {
-  //   let { from, to, text } = view.state.doc.lineAt(pos);
-  //   let start = pos,
-  //     end = pos;
-  //   while (start > from && /\w/.test(text[start - from - 1])) start--;
-  //   while (end < to && /\w/.test(text[end - from])) end++;
-  //   if ((start == pos && side < 0) || (end == pos && side > 0)) return null;
-  //   return {
-  //     pos: start,
-  //     end,
-  //     above: true,
-  //     create(view) {
-  //       let dom = document.createElement("div");
-  //       dom.textContent = text.slice(start - from, end - from);
-  //       return { dom };
-  //     },
-  //   };
-  // });
 
   let hoverCounter = 0;
   let lastHoverCounter = 0;
