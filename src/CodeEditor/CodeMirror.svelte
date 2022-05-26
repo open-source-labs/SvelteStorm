3d84827d53bb48ff1c853c7e625eacdb9478aa42
Ricky Johnson <rickyjohnson@Rickys-MBP.lan>
2022-05-26 17:23:41 -0400

    TS merge

--- src/CodeEditor/CodeMirror.svelte
+++ src/CodeEditor/CodeMirror.svelte
@@ -1,4 +1,5 @@
-<script lang="ts">
+<!-- <script lang="ts"> -->
+<script>
   import { afterUpdate, onMount } from "svelte";
   import CodeMirror from "codemirror";
   import { scale } from "svelte/transition";
