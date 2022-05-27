e0c0ed8b9ad2479dab1f1b1aef0c68319e466dfa
Ricky Johnson <rickyjohnson@Rickys-MBP.lan>
2022-05-27 17:53:53 -0400

    merged directorystore

--- src/Utilities/DirectoryStore.ts
+++ src/Utilities/DirectoryStore.ts
@@ -1,39 +1,40 @@
-import {writable} from 'svelte/store';
+import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 //Move to separate TypeScript file
 
-import type {NewFile, EditorCacheType, State} from '../types'
-
-
-
-
-
+import type { NewFile, EditorCacheType, State } from '../types';
 
 const DirectoryData: Writable<State> = writable<State>({
   mainDir: '',
   fileTree: [],
-  openFilePath :'',
+  openFilePath: '',
   fileRead: false,
   stateObj: {},
   activeFile: '',
   rename: false,
   deleteFile: false,
-  activeDir : '',
+  activeDir: '',
   createFile: false,
   createFolder: false,
   createMainFile: false,
   createMainFolder: false,
   reload: false,
-  activeFolder : ''
+  activeFolder: '',
 });
 
-console.log(DirectoryData)
+console.log(DirectoryData);
 const openTabs: Writable<NewFile[]> = writable<NewFile[]>([]);
 
 const currentTabFilePath: Writable<string> = writable<string>('');
 
-const editorCache: Writable<EditorCacheType> = writable<EditorCacheType> ({});
+const editorCache: Writable<EditorCacheType> = writable<EditorCacheType>({});
 
 const codeMirrorEditor = writable(undefined);
 
-export { DirectoryData, openTabs, editorCache, codeMirrorEditor, currentTabFilePath };
+export {
+  DirectoryData,
+  openTabs,
+  editorCache,
+  codeMirrorEditor,
+  currentTabFilePath,
+};
