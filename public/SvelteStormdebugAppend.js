4ce18d85ca0d856e8a3377a6a39b0cc61e2c2e83
Elliott <Elliott@Elliotts-MacBook-Pro.local>
2022-07-21 12:00:09 -0600

    notes on updating window to reflect snapshot

--- public/SvelteStormdebugAppend.js
+++ public/SvelteStormdebugAppend.js
@@ -21,6 +21,27 @@ window.addEventListener(
   false
 );
 
+/* inside visualization file
+const instance = {
+  message: "TIME_TRAVEL",
+  ctxIndex: the index of the snapshot (button clicked)
+}
+const visualizationWindow = BrowserWindow.getFocusedWindow();
+visualizationWindow.webContents.send('TIME_TRAVEL', instance)
+
+THEN CHANGE THIS EVENT LISTENER TO ------->
+ ipcRender.on('TIME_TRAVEL', (event, data) => {
+  if (data.message === 'TIME_TRAVEL') {
+      const i = data.ctxIndex;
+      lastIndex = i;
+      if (cacheState[i]) {
+        cacheState[i].forEach((componentState) => {
+          componentState[0].$inject_state(componentState[1]);
+        });
+      }
+    }
+})
+*/
 /*
 * =========================================================
 *   The Above is SvelteStorm 4.0 Debug Monitoring Code.
