00bd946da4661537cdc5e2487d7a687c1b67b0ce
Jim White <Jim.White@me.com>
2022-07-31 22:37:28 -0400

    Visual Cleanup + Folder Icons
    Co-authored-by: Elliott <elliottburr@users.noreply.github.com>
    Co-authored-by: 8-ryan-8 <8-ryan-8@users.noreply.github.com>

---
+++ src/Version4UtilityFunctions/wrap-app-functions.ts
@@ -0,0 +1,100 @@
+// import type {Filetree} from '../types';
+
+const {ipcRenderer} = require('electron');
+const myPath = require('node:path');
+const npmAddScript = require('npm-add-script');
+const process = require('process');
+const fs = require('fs');
+
+/*
+ * ==================================================
+ *   Hard coded file paths & names
+ * ==================================================
+ */
+
+export function updatePackageJson(path) {
+  const currentDir = process.cwd();
+  try {
+    // Change the directory
+    process.chdir(path);
+    ipcRenderer.send('terminal-into', `cd ${path}\r`);
+
+    console.log('directory has successfully been changed');
+  } catch (err) {
+    // Printing error if occurs
+    console.error('error while changing directory');
+  }
+  var content: string = fs.readFileSync(`package.json`).toString();
+  if (!content.match(/sdebug/gm)) {
+    try {
+      npmAddScript({
+        key: 'sdebug',
+        value: 'rollup --config rollup.config.new.js -w',
+        force: true,
+      });
+      console.log('Added sdebug script to package.json');
+    } catch (err) {
+      console.error(`${err}error while adding sdebug script to package.json`);
+    }
+  }
+  process.chdir(currentDir);
+}
+
+/*
+ * ==================================================
+ * Rename rollup.config.js to rollup.config.original.js
+ *
+ * Make copy of projects current rollup config file
+ *
+ * Add two lines to beginning of rollup.config.js
+ *   import path
+ *   import rollup plugin
+ *
+ * Add banner needed lines in plugin section
+ *
+ * ==================================================
+ */
+export function updateRollupConfig(path) {
+  const originalConfig = path + '/rollup.config.js';
+  const newConfig = path + '/rollup.config.new.js';
+
+  const myFileContent = fs.readFileSync(originalConfig, 'utf8');
+  const myFileAsArray = myFileContent.split('\n');
+
+  // As each line is examined we either push it to a new array OR add our lines THEN continue loop and pushing to new Array
+  // When done new Array will be written to new file.
+  const newMyFileAsArray = [];
+  let addedImports = false;
+
+  for (let i = 0; i < myFileAsArray.length; i++) {
+    const daLine = myFileAsArray[i].split(' ');
+    if (daLine[0] !== 'import' && !addedImports) {
+      newMyFileAsArray.push(
+        `\timport banner from '${myPath.resolve(
+          __dirname,
+          '../src/Version4UtilityFunctions/rollup-plugin-pre-app-end'
+        )}';`
+      );
+      newMyFileAsArray.push(`const path = require('path');`);
+      newMyFileAsArray.push(myFileAsArray[i]);
+      addedImports = true;
+    } else if (myFileAsArray[i].trim() === 'plugins: [') {
+      newMyFileAsArray.push(myFileAsArray[i]);
+      newMyFileAsArray.push(`    banner({`);
+      newMyFileAsArray.push(`      tprependFile: '${myPath.resolve(__dirname, 'SvelteStormdebugPrepend.js')}',` );
+      newMyFileAsArray.push(`      tappendFile: '${myPath.resolve(__dirname, 'SvelteStormdebugAppend.js')}',`);
+      newMyFileAsArray.push(`      tencoding: 'utf-8', // default is utf-8`);
+      newMyFileAsArray.push(`    }),`);
+    } else {
+      newMyFileAsArray.push(myFileAsArray[i]);
+    }
+  }
+
+  const textToWrite = newMyFileAsArray.join('\n');
+
+  fs.writeFileSync(newConfig, textToWrite, {
+    encoding: 'utf8',
+    flag: 'w',
+    mode: 0o666,
+  });
+}
