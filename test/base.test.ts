5614462e0a7e4d9dc72d4cf96bd84817e68d23d7
Elliott Burr <elliottnburr@gmail.com>
2022-08-24 14:39:21 -0700

    Co-authored-by: Karen Cheung <karenc810@users.noreply.github.com> Co-authored-by: Ryan Huie <8-ryan-8@users.noreply.github.com>

--- test/base.test.ts
+++ test/base.test.ts
@@ -1,5 +1,14 @@
 import { _electron as electron } from "playwright";
 import { test, expect } from "@playwright/test";
+import { 
+  clickMenuItemById,
+  findLatestBuild, 
+  ipcMainCallFirstListener, 
+  ipcRendererCallFirstListener, 
+  parseElectronApp,
+  ipcMainInvokeHandler,
+  ipcRendererInvoke
+} from 'electron-playwright-helpers'
 
 test("Launch electron app", async () => {
   // Launch electron app
@@ -51,12 +60,36 @@ test.describe("Check Main Page", async () => {
     expect(title).toBe("SvelteStorm");
   });
 
-  test("Check version number: APP", async () => {
-    const versionNumberApp = await firstWindow.innerText("data-testid=version-number-app");
-    expect(versionNumberApp).not.toBe("-");
-    const isValidNumberApp = semver.valid(semver.coerce(versionNumberApp));
-    expect(semver.valid(isValidNumberApp)).not.toBeNull();
-  });
+  test('Trigger IPC listener via main process', async () => {
+    electronApp.evaluate(({ ipcMain }) => {
+      ipcMain.emit('new-window')
+    })
+    const newPage = await electronApp.waitForEvent('window')
+    expect(newPage).toBeTruthy()
+    firstWindow = newPage
+  })
+
+  test('Send IPC message from Renderer to Main and open new window', async () => {
+    // evaluate this script in render process
+    // requires webPreferences.nodeIntegration true and contextIsolation false
+    await firstWindow.evaluate(() => {
+      require('electron').ipcRenderer.send('openDebugAppWindow', '8080')
+    })
+    
+    const newPage = await electronApp.waitForEvent('window')
+    expect(newPage).toBeTruthy()
+    firstWindow = newPage
+  })
+  
+  test('receive synchronous data via ipcMainCallFirstListener()', async () => {
+    const data = await ipcMainCallFirstListener(electronApp, 'main-synchronous-data')
+    expect(data).toBe('Main Synchronous Data')
+  })
+  
+  test('receive asynchronous data via ipcMainCallFirstListener()', async () => {
+    const data = await ipcMainCallFirstListener(electronApp, 'main-asynchronous-data')
+    expect(data).toBe('Main Asynchronous Data')
+  })
 
   test.afterAll(async () => {
     await electronApp.close();
